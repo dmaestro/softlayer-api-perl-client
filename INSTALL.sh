e8c2c36c12c85d7e2feba6dd81e0721803d865ec
Douglas Schrag <dschrag@hostgator.com>
2015-05-04 12:58:39 -0500

    More verbose, please

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,7 +3,8 @@ perl_bin=${PERLBREW_PATH##*:}
 if [ -d $perl_bin ] ; then
     perl_root=${perl_bin%/*}
     version=${perl_root##*-}
-    [ -d SoftLayer ] && tar cf - SoftLayer | tar -xf - -C $perl_root/lib/site_perl/$version
+    echo "Attempting install of SoftLayer/API to $perl_root/lib/site_perl/$version"
+    [ -d SoftLayer ] && tar cf - SoftLayer | tar -xvf - -C $perl_root/lib/site_perl/$version
 else
     echo "PERLBREW directory not found: $PERLBREW_PATH"
     exit 1
